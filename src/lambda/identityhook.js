8cf19e17870b1562d0c472b42c57f7afe89b03a3
Shrey <shrey.dabhi23@gmail.com>
2018-11-04 09:59:59 +0530

    add event log trigger

--- src/lambda/identityhook.js
+++ src/lambda/identityhook.js
@@ -38,35 +38,30 @@ exports.handler = (event, context,  callback) => {
     console.log(body);
     console.log(responseBody);
 
-    callback(null, {
-      statusCode: 200,
-      body: responseBody
+    await axios({
+      url: 'https://count-em-all-db.herokuapp.com/v1alpha1/graphql',
+      method: 'post',
+      data: {
+        query: `
+          mutation {
+              insert_users(objects: [{id: "${body.user.id}", name: "${body.user.user_metadata.full_name}", email: "${body.user.email}"}]) {
+                affected_rows
+              }
+            }
+          `
+      }
+    }).then(res => {
+      callback(null, {
+        statusCode: 200,
+        body: responseBody
+      });
+    }).catch((err) => {
+      console.log(err)
+      callback(null, {
+        statusCode: 400,
+        body: responseBody
+      });
     });
-
-    // axios({
-    //   url: 'https://count-em-all-db.herokuapp.com/v1alpha1/graphql',
-    //   method: 'post',
-    //   data: {
-    //     query: `
-    //       mutation {
-    //           insert_users(objects: [{id: "${body.user.id}", name: "${body.user.user_metadata.full_name}", email: "${body.user.email}"}]) {
-    //             affected_rows
-    //           }
-    //         }
-    //       `
-    //   }
-    // }).then(res => {
-    //   return {
-    //     statusCode: 200,
-    //     body: responseBody
-    //   };
-    // }).catch((err) => {
-    //   console.log(err)
-    //   return {
-    //     statusCode: 400,
-    //     body: responseBody
-    //   };
-    // });
   } else {
     callback(null, {
       statusCode: 200
