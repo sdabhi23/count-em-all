8009e0b268ddcdbfd941b55676451346aac6990e
Shrey <shrey.dabhi23@gmail.com>
2018-11-29 22:39:52 +0530

    update webhook

--- src/lambda/jwt-hook.js
+++ src/lambda/jwt-hook.js
@@ -1,43 +1,15 @@
-/*
- * This webhook is called whenever a new user tries to sign up on the site using the Netlify Identity Wisget.
- * It checks that the user's email is on a permitted domain (such as username@ntti3.io),
- * and will only allow new users to sign up from permitted domains. Upon a valid signup, the new user is
- * automatically given the 'reader' role, so that they may view everything on the website.
- *
- * To get this script called by Netlify, you need to activate the webhook, here are the steps:
- *     - go to the Netlify Site Settings for your web site
- *     - look for the Settings -> Identity -> Notifications section
- *     - edit the Webhook
- *     - set the URL to https://<your-site-name>.netlify.com/.netlify/functions/webhook
- *     - leave the Secret field blank
- *     - check the event box for validate
- *     - click on the Save button
- *
- * In addition, the site should have a _redirects file that limits access to those with particular roles.
- * This will restrict visitor access to only those people with a role that permits access.
- *
- * Here's what the _redirects file should look like for your static website when you publish it on Netlify:
- *
- *     /test/* 200! Role=reader
- *     /test/ /login 404
- *
- * (replace test with the name of your protected sub-folder, and login with the name of the fail html file)
- *
- * More information about Netlify Identity service and Functions here:
- *     - https://www.netlify.com/docs/identity/
- *     - https://www.netlify.com/docs/functions/#identity-and-functions
- */
-
-import axios from "axios";
-
 exports.handler = (event, context, callback) => {
   const {identity, user} = context.clientContext;
 
   console.log(identity);
   console.log(user);
 
-  callback(null, {
-    statusCode: 200,
-    body: JSON.stringify(user)
-  });
+  var data = {
+    "x-hasura-user-id": user.sub,
+    "x-hasura-role": "user",
+  }
+
+  console.log(data);
+
+  callback(null, data);
 };
